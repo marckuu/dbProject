a63826963d20663809629e9f0b43060ccb0a94a3
Mark <markisme6383@gmail.com>
2024-12-05 23:46:58 +0300

    Теперь точно выводит списки и почти позволяет редактировать

--- src/main/java/com/markuu/demo/controllers/MainController.java
+++ src/main/java/com/markuu/demo/controllers/MainController.java
@@ -6,6 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.sql.Date;
+
 
 @Controller
 public class MainController {
@@ -33,7 +40,7 @@ public class MainController {
     private String allPatient(Model model) {
         Iterable<Patient> patient = patientRepository.findAll();
         model.addAttribute("patients", patient);
-        return "testmain";
+        return "mainPage";
     }
 
     @GetMapping("/getChildrens")
@@ -72,7 +79,62 @@ public class MainController {
     }
     @GetMapping("/")
     private String page(Model model) {
-        return "testmain";
+        return "mainPage";
+    }
+
+    @GetMapping("/mainPage")
+    private String mainPage(Model model) {
+        Iterable<Patient> patient = patientRepository.findAll();
+        model.addAttribute("patients", patient);
+        return "mainPage";
+    }
+
+    @GetMapping("/childPage")
+    private String childPage(Model model) {
+        Iterable<Child> child = childRepository.findAll();
+        model.addAttribute("childs", child);
+        return "childPage";
+    }
+
+    @GetMapping("/wardPage")
+    private String wardPage(Model model) {
+        Iterable<Ward> ward = wardRepository.findAll();
+        model.addAttribute("wards", ward);
+        return "wardPage";
+    }
+
+    @GetMapping("/hospPage")
+    private String hospPage(Model model) {
+        Iterable<Hospitalization> hospitalization = hospitalizationRepository.findAll();
+        model.addAttribute("hospitalizations", hospitalization);
+        return "hospPage";
+    }
+
+    @GetMapping("/childbPage")
+    private String childbPage(Model model) {
+        Iterable<Childbirth> childbirth = childbirthRepository.findAll();
+        model.addAttribute("childbirths", childbirth);
+        return "childPage";
+    }
+
+    @GetMapping("/employeePage")
+    private String employeePage(Model model) {
+        Iterable<Employee> employee = employeeRepository.findAll();
+        model.addAttribute("employees", employee);
+        return "employeePage";
+    }
+
+
+
+
+
+
+    @PostMapping("/modifyPatient")
+    private String modifyPatient(@RequestParam String firstName, @RequestParam String middleName, @RequestParam String lastName, @RequestParam String bloodType,
+                                 @RequestParam String mobilePhone, @RequestParam String address, @RequestParam String medicalHistory, @RequestParam Long ward, Model model, @RequestParam String birthDate, @RequestParam Long id) throws ParseException {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        patientRepository.set(firstName, middleName, lastName, address, mobilePhone, birthDate, bloodType, medicalHistory, ward, id);
+        return "mainPage";
     }
 
 }
--- src/main/java/com/markuu/demo/repo/PatientRepository.java
+++ src/main/java/com/markuu/demo/repo/PatientRepository.java
@@ -1,11 +1,14 @@
 package com.markuu.demo.repo;
 
 import com.markuu.demo.models.Patient;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+import java.util.Date;
 import java.util.List;
 
 @Repository
@@ -13,7 +16,20 @@ public interface PatientRepository extends CrudRepository<Patient, Long> {
 
     @Query("SELECT u FROM Patient u")
     List<Patient> findAll();
+//
+//    @Modifying
+//    @Query("INSERT INTO Patient (first_name, middle_name, last_name, address, mobile_phone, birth_date, blood_type, medical_history, ward) VALUES (:first_name, :middle_name, :last_name, :address, :mobile_phone, :birth_date, :blood_type, :medical_history, :ward)")
+//    void modify (@Param("first_name") String first_name, @Param("middle_name") String middle_name, @Param("last_name") String last_name, @Param("address") String address,
+//    @Param("mobile_phone") String mobile_phone, @Param("birth_date") String birth_date, @Param("blood_type") String blood_type, @Param("medical_history") String medical_history,
+//                 @Param("ward") String ward);
+//
 
-
-
+    @Modifying
+    @Transactional
+    @Query("UPDATE Patient SET first_name = :first_name, middle_name = :middle_name, last_name = :last_name, address = :address, mobile_phone = :mobile_phone, birth_date = :birth_date, blood_type = :blood_type, medical_history = :medical_history, ward.id = :ward WHERE id = :id")
+    void set(@Param("first_name") String first_name, @Param("middle_name") String middle_name, @Param("last_name") String last_name, @Param("address") String address,
+             @Param("mobile_phone") String mobile_phone, @Param("birth_date") String birth_date, @Param("blood_type") String blood_type, @Param("medical_history") String medical_history,
+             @Param("ward") Long ward, @Param("id") Long id);
 }
+
+
